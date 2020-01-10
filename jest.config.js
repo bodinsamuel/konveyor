66b431ddc182f58a3ee424416426989e73b30863
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2020-01-10 20:47:22 +0100

    more steps in CI

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line import/no-commonjs
+module.exports = {
+  testEnvironment: 'node',
+
+  name: 'unit',
+  displayName: 'konveyor',
+
+  preset: 'ts-jest',
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+    },
+  },
+};
